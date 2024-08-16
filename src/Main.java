f65ac31caa2043ab1476eff7f08242f925142723
herreang000 <herreang000@warren.k12.in.us>
2024-08-16 12:23:05 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int kilobytes = printMegaBytesAndKiloBytes(5000);
+
+    }
+
+    public static int printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes >= 0) {
+            int megaBytes = kiloBytes / 1024;
+            int kb = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB= " + megaBytes + " MB and " + kb + " KB");
+        } else {
+            System.out.println("Invalid Value");
+        }
+
+        return kiloBytes;
+    }
+
+
 }
